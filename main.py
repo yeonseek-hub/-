cddb5ef481512ad4b8a0cf0ef54c244a8145cd21
yeonyeon <yeonseek682@gmail.com>
2025-11-05 09:12:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
-st.text_input('이름을 입력해 주세요')
-st.button('인사말 생성')
+a=st.text_input('이름을 입력해 주세요')
+if st.button('인사말 생성'):
+  st. wirte(a+'님 안녕 반갑습니다!')
